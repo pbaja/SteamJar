cd328d124480d02d729a32ad3cde0bd2d306378f
pbaja <github@pbaja.me>
2022-06-08 13:26:36 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
 # Configuration
+BRANCH="dev"
 INSTALLATION_DIR=~/SteamJar
-REPOSITORY_URL="https://raw.githubusercontent.com/pbaja/SteamJar/main"
+REPOSITORY_URL="https://raw.githubusercontent.com/pbaja/SteamJar/$BRANCH"
 
 # Liftoff
 echo '=> Welcome to the SteamJar installer!'
 
-# # Check installed version
-# installed_version=0
-# if [[ -f $INSTALLATION_DIR/version.txt ]]; then
-#     installed_version=$(cat $INSTALLATION_DIR/version.txt)
-#     echo "-> Installed version: "$installed_version
-#     installed_version=$(sed 's/[^0-9]*//g' <<< "$installed_version")
-# else
-#     echo "-> No installation found"
-# fi
+# Check installed version
+installed_version=0
+if [[ -f $INSTALLATION_DIR/version.txt ]]; then
+    installed_version=$(cat $INSTALLATION_DIR/version.txt)
+    echo "-> Installed version: "$installed_version
+    installed_version=$(sed 's/[^0-9]*//g' <<< "$installed_version")
+else
+    echo "-> No installation found"
+fi
 
-# # Check latest version
-# latest_version=$(curl -s -f $REPOSITORY_URL/version.txt)
-# if [[ "$?" -ne "0" ]]; then
-#     echo "-> Failed to check latest version"
-#     latest_version=0
-#     exit 1
-# else
-#     echo "-> Latest version: "$latest_version
-#     latest_version=$(sed 's/[^0-9]*//g' <<< "$latest_version")
-# fi
+# Check latest version
+latest_version=$(curl -s -f $REPOSITORY_URL/version.txt)
+if [[ "$?" -ne "0" ]]; then
+    echo "-> Failed to check latest version"
+    latest_version=0
+    exit 1
+else
+    echo "-> Latest version: "$latest_version
+    latest_version=$(sed 's/[^0-9]*//g' <<< "$latest_version")
+fi
 
-# # Check if a latest version is newer
-# if [[ "$installed_version" -ge "$latest_version" ]]; then
-#     echo "-> Latest version is already installed"
-#     exit 0
-# fi
+# Check if a latest version is newer
+if [[ "$installed_version" -ge "$latest_version" ]]; then
+    echo "-> Latest version is already installed"
+    exit 0
+fi
 
 # Remove old files, create new directory
 echo "-> Removing previous installation if exists"
@@ -41,7 +42,11 @@ mkdir "$INSTALLATION_DIR"
 
 # Download latest zip
 echo "-> Downloading latest version..."
-curl -sL 'https://github.com/pbaja/SteamJar/archive/refs/heads/main.zip' --output "/tmp/SteamJar.zip"
+curl -fsL "https://github.com/pbaja/SteamJar/archive/refs/heads/$BRANCH.zip" --output "/tmp/SteamJar.zip"
+if [[ "$?" -ne "0" ]]; then
+    echo "-> Failed to download"
+    exit 2
+fi
 
 # Unzip
 echo "-> Unpacking archive"
@@ -50,8 +55,8 @@ rm /tmp/SteamJar.zip
 
 # Move to installation directory
 echo "-> Moving files"
-mv /tmp/SteamJar-main/* "$INSTALLATION_DIR"
-rm -r /tmp/SteamJar-main
+mv "/tmp/SteamJar-$BRANCH"/* "$INSTALLATION_DIR"
+rm -r "/tmp/SteamJar-$BRANCH"
 
 # Link desktop shortcut
 echo "-> Creating desktop shortcut"
